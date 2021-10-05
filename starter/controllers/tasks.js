fde73b43433467d6e1c0eceea870a29545c5b9ac
trieungochai <trieungochai.dev@gmail.com>
2021-10-05 15:19:49 +0700

    All Routes

--- starter/controllers/tasks.js
+++ starter/controllers/tasks.js
@@ -1,5 +1,21 @@
 const getAllTasks = (req, res) => {
-  res.send("All items from the file.");
+  res.send("Get all the tasks");
 };
 
-module.exports = { getAllTasks };
+const getTask = (req, res) => {
+  res.send("Get single task");
+};
+
+const createTask = (req, res) => {
+  res.send("Create a new task");
+};
+
+const updateTask = (req, res) => {
+  res.send("Update task");
+};
+
+const deleteTask = (req, res) => {
+  res.send("Delete task");
+};
+
+module.exports = { getAllTasks, getTask, createTask, updateTask, deleteTask };
