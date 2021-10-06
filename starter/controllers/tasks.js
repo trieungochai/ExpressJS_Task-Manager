11aeb2b4ff454281b58a98f263a13d38ab34330c
trieungochai <trieungochai.dev@gmail.com>
2021-10-06 23:08:37 +0700

    Delete Tasl

--- starter/controllers/tasks.js
+++ starter/controllers/tasks.js
@@ -33,7 +33,18 @@ const createTask = async (req, res) => {
   }
 };
 
-const updateTask = (req, res) => {
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskID}` });
+    }
+
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
   res.send("Update task");
 };
 
