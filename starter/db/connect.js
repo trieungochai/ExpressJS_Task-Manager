f4699982008f6675cc4347231009c03a190327e3
trieungochai <trieungochai.dev@gmail.com>
2021-10-06 18:51:47 +0700

    ENV VARS

--- starter/db/connect.js
+++ starter/db/connect.js
@@ -1,10 +1,7 @@
 const mongoose = require("mongoose");
 
-const connectionString =
-  "mongodb+srv://john_doe:<password>@nodeexpressprojects.budye.mongodb.net/TASK-MANAGER?retryWrites=true&w=majority";
-
 const connectDB = (url) => {
-  return mongoose.connect(connectionString, {
+  return mongoose.connect(url, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
