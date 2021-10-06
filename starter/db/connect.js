7f6a8d3ebc2cac93897052fdc90eb98401f509ee
trieungochai <trieungochai.dev@gmail.com>
2021-10-06 18:30:42 +0700

    Connect to DB

--- starter/db/connect.js
+++ starter/db/connect.js
@@ -1,2 +1,14 @@
+const mongoose = require("mongoose");
+
 const connectionString =
-  "mongodb+srv://john_doe:<password>@nodeexpressprojects.budye.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  "mongodb+srv://john_doe:<password>@nodeexpressprojects.budye.mongodb.net/TASK-MANAGER?retryWrites=true&w=majority";
+
+mongoose
+  .connect(connectionString, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to the Database..."))
+  .catch((err) => console.log(err));
