bfc44f02e0cb215d2d5cfb3aec06e226acd844b4
trieungochai <trieungochai.dev@gmail.com>
2021-10-06 18:46:10 +0700

    Refactor Connect

--- starter/app.js
+++ starter/app.js
@@ -1,7 +1,7 @@
-require("./db/connect");
 const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
 
 // middleware
 app.use(express.json());
@@ -15,6 +15,15 @@ app.use("/api/v1/tasks", tasks);
 
 const port = 3000;
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}...`);
-});
+const start = async () => {
+  try {
+    await connectDB();
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
--- starter/db/connect.js
+++ starter/db/connect.js
@@ -3,12 +3,13 @@ const mongoose = require("mongoose");
 const connectionString =
   "mongodb+srv://john_doe:<password>@nodeexpressprojects.budye.mongodb.net/TASK-MANAGER?retryWrites=true&w=majority";
 
-mongoose
-  .connect(connectionString, {
+const connectDB = (url) => {
+  return mongoose.connect(connectionString, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
-  })
-  .then(() => console.log("Connected to the Database..."))
-  .catch((err) => console.log(err));
+  });
+};
+
+module.exports = connectDB;
